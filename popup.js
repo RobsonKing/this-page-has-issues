dfe6e9f61da0a913244bf4508c6799f983f6df29
robson king <robson.king@gmail.com>
2021-11-09 21:33:59 -0700

    feat: initial file

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import { render, h } from 'preact';
-import GitHub from 'github-api';
 import {useEffect, useState} from "preact/hooks";
-// import fetch from "node-fetch";
 const token = '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';
 
 const query = `
@@ -22,66 +23,36 @@ const query = `
   }
   }`;
 
+// todo rmk (09 Nov. 2021): page info?
+
 function Repos() {
-    const [repos, setRepos] = useState([]);
-    // const increment = useCallback(() => {
-    //     setValue(value + 1);
-    // }, [value]);
+    const [issues, setIssues] = useState([]);
 
     useEffect(() => {
-        fetch('https://api.github.com/graphql', {
-            method: 'POST',
-            body: JSON.stringify({query}),
-            headers: {
-                'Authorization': `Bearer ${token}`,
-            },
-        }).then(res => res.text())
-            .then(body => console.log(body)) // {"data":{"repository":{"issues":{"totalCount":247}}}}
-            .catch(error => console.error(error));
-// todo rmk (08 Nov. 2021):
-//        - parse body
-//        - use await
-//        - display results
-//        - WhereTheBug
-//        - test searching for url
-//        - does regex help?
-//        - menu item to get page url
-//        - menu item to log bug
-//        - link to issue
-//        - preview issue
-//        - closed vs open issues
-//        - add extra search params
-//        - bring over: menu code
-//        - save token
-//        - save URL mapping
-//        - abstract the issue module
-//        - abstract an issue
-//        - search limit... paging...
+        const  getData= async () => {
+            let response = await fetch('https://api.github.com/graphql', {
+                method: 'POST',
+                body: JSON.stringify({query}),
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            })
+            if (response.status === 200){
+                let data = await response.json();
+                console.log("*** data ***", data); // todo rmk (09 Nov. 2021): remove
+                setIssues(data.data.search.nodes)
 
+            } else{
+                // todo rmk (09 Nov. 2021):
+                throw "???"
+            }
+            console.log("*** RMK ***", response); // todo rmk (09 Nov. 2021): remove
+                // .then(res => res.text())
+                // .then(body => console.log(body)) // {"data":{"repository":{"issues":{"totalCount":247}}}}
+                // .catch(error => console.error(error));
+        }
 
-        // Trigger your effect
-        // console.log("*** here we go! ***", ); // todo rmk (07 Nov. 2021): remove
-        // const gh = new GitHub({
-        //     token: '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'
-        // });
-        //
-        // let me = gh.getUser();
-        // me.listStarredRepos(function(err, repos) {
-        //     // look at all the starred repos!
-        //     console.log("*** err ***", err); // todo rmk (07 Nov. 2021): remove
-        //     console.log("*** repos ***", repos); // todo rmk (07 Nov. 2021): remove
-        //     setRepos(repos)
-        // });
-
-        // let search = gh.search()
-        //
-        // console.log("*** RMK 3***", search); // todo rmk (08 Nov. 2021): remove
-        //
-        // search.forIssues(function(err, results) {
-        //         console.log("*** err ***", err); // todo rmk (07 Nov. 2021): remove
-        //         console.log("*** repos ***", results); // todo rmk (07 Nov. 2021): remove
-        //         setRepos(results)
-        //     })
+        getData();
         return () => {
             // Optional: Any cleanup code
         };
@@ -90,7 +61,7 @@ function Repos() {
     return (
         <div>
            Repos
-            {repos.map( (repo)=><div>{repo.name}</div>)}
+            {issues.map( (repo)=><div>{repo.title}</div>)}
         </div>
     );
 }
---
+++ readme.md
@@ -0,0 +1,19 @@
+## todo rmk (08 Nov. 2021):
+ - search
+    - get url from page
+    - build query
+    - test searching for url
+    - does regex help?
+  - menu item to get page url
+  - menu item to log bug
+  - link to issue
+  - preview issue
+  - closed vs open issues
+  - add extra search params
+  - bring over: menu code
+  - save token
+ - save URL mapping
+ - abstract the issue module
+ - abstract an issue
+ - search limit... paging...
+ - tests
