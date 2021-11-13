4f2d0d6128b351b33a2f1131b8af7b8bfe504ef5
robson king <robson.king@gmail.com>
2021-11-12 22:18:27 -0700

    refactor: abstacting - issue model - issue provider

--- popup.js
+++ popup.js
@@ -1,108 +1,44 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import { render, h } from 'preact';
+import {render, h} from 'preact';
 import {useEffect, useState} from "preact/hooks";
-const token = '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';
-
-const PAGE = "*PAGE*"
-//https://docs.github.com/en/search-github/searching-on-github/searching-issues-and-pull-requests
-// todo rmk (10 Nov. 2021): is:open
-// todo rmk (10 Nov. 2021):sort https://docs.github.com/en/search-github/getting-started-with-searching-on-github/sorting-search-results
-// todo rmk (09 Nov. 2021): page info?
-// todo rmk (10 Nov. 2021):is there a better way to build the url?  git hub ignores special characters, quotes mean the
-//  words must be together, but need and end so it doesn't find child pages or other URLs
-const query = `
-  query {
-     search(first:5,query:"repo:atvenu/atvenu \\"start(${PAGE})end\\"",type:ISSUE){
-    nodes{
-      __typename
-      ... on Issue{
-        number
-        milestone{
-          title
-        }
-        state
-        title
-        author{
-          login          
-        }
-        url
-      }
-    }
-  }
-  }`;
+import {githubIssueService} from "./src/services/IssueService";
 
 function Issues() {
     const [issues, setIssues] = useState([]);
-    const [currentPage, setCurrentPage] = useState("");
 
     useEffect(() => {
-        const  getData= async () => {
-            let queryOptions = { active: true, currentWindow: true };
-            let [tab] = await chrome.tabs.query(queryOptions);
-            const url = new URL(tab.url);
-            let cP =(`${url.pathname}${url.search}`).replace(/\d+/g,'x');
-            setCurrentPage(cP);
-
-            let pageQuery = query.replace(PAGE,cP)
-            console.log("*** pageQuery ***",typeof pageQuery,pageQuery); // todo rmk (10 Nov. 2021): remove
-            let response = await fetch('https://api.github.com/graphql', {
-                method: 'POST',
-                body: JSON.stringify({query: pageQuery}),
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                },
-            })
-            if (response.status === 200){
-                let data = await response.json();
-                if (data.errors){
-                    console.log("*** errors ***", data.errors); // todo rmk (10 Nov. 2021): remove
-                } else {
-                    console.log("*** data ***", data); // todo rmk (09 Nov. 2021): remove
-                    setIssues(data.data.search.nodes)
-                }
-
-            } else{
-                // todo rmk (09 Nov. 2021):
-                console.log("*** error ***", response); // todo rmk (10 Nov. 2021): remove
-                throw "???"
-            }
+        const getData = async () => {
+            setIssues(await githubIssueService.getIssuesForCurrentPage());
         }
 
         // noinspection JSIgnoredPromiseFromCall
         getData();
-        return () => {
-            // Optional: Any cleanup code
-        };
     }, []);
 
-    // todo rmk (11 Nov. 2021): truncate long titles
-    //  -
     return (
-        <div style={{display:'flex',flexDirection:"column",width:600}}>
-            {/*Current Page*/}
-            {/*{currentPage}*/}
-            <div style={{fontWeight:"Bold",fontsize:"20"}}>Issues</div>
-            <div style={{display:'flex',flexDirection:"column"}}>
-                {issues.map( (issue)=><div style={{
-                    display:'flex',
-                    flexDirection:"row",
+        <div style={{display: 'flex', flexDirection: "column", width: 600}}>
+            <div style={{fontWeight: "Bold", fontsize: "20"}}>Issues</div>
+            <div style={{display: 'flex', flexDirection: "column"}}>
+                {issues.length === 0 && <div style={{color: 'orange', fontStyle: 'italic'}}>No issues found</div>}
+                {issues.map((issue) => <div style={{
+                    display: 'flex',
+                    flexDirection: "row",
                     borderStyle: "solid",
                     borderWidth: 1,
-                    borderColor:'lightgrey',
-                    padding:5,
+                    borderColor: 'lightgrey',
+                    padding: 5,
                 }}>
-                    <div style={{flex:"1", color: issue.state === "OPEN" ? 'green' : 'purple'}}>{issue.number}</div>
-                    {!!issue.milestone && <div style={{flex:"2"}}>{issue.milestone.title}</div>}
-                    <div style={{flex:"5"}}>
+                    <div style={{flex: "1", color: issue.isOpen ? 'green' : 'purple'}}>{issue.number}</div>
+                    <div style={{flex: "2"}}>{issue.milestone}</div>
+                    <div style={{flex: "5"}}>
                         <a href={issue.url} target="_blank">
                             {issue.title}
                         </a>
                     </div>
                 </div>)}
             </div>
-
         </div>
     );
 }
--- readme.md
+++ readme.md
@@ -1,20 +1,29 @@
 ## todo rmk (08 Nov. 2021):
-  - menu item to get page url
-  - menu item to log bug
-  - preview issue
-  - filter closed vs open issues
-  - add extra search params
-  - bring over: menu code
-  - save token
- - save URL mapping
- - abstract the issue module
- - abstract an issue
- - search limit... paging...
- - tests
- - record what url = what repo (staging, dev, etc)
- - milestone!
- - copy URL and create bug
- - group by bug, issues, etc
- - alias routes
- - fall back to wild card search
- - how to store css in preact
+
+- menu item to get page url
+- menu item to log bug
+- preview issue
+- filter closed vs open issues
+- add extra search params
+- bring over: menu code
+- save token
+- save URL mapping
+- search limit... paging...
+- tests
+- record what url = what repo (staging, dev, etc)
+-
+    * issue assigned to
+- copy URL and create bug
+- group by bug, issues, etc
+- alias routes
+- fall back to wild card search
+-
+    * how to store css in preact
+- search by page title
+- search by location on page (right click)
+    - show tree of nodes? let the use select
+- typescript?
+- axios? what does this do for me?
+- respect browser dark mode
+-
+    * spinner while loading
